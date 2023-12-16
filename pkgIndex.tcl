12284c56a01cff1730e7353e494bf0955bb8ae02
Danilo Chang <ray2501@gmail.com>
2023-12-16 20:56:02 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,8 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded monetdb 0.9.15 \
-    [list load [file join $dir libmonetdb0.9.15.so] monetdb]
-
-package ifneeded tdbc::monetdb 0.9.15 \
-    [list source [file join $dir tdbcmonetdb.tcl]]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded monetdb 0.9.15 \
+	    [list load [file join $dir libtcl9monetdb0.9.15.so] [string totitle monetdb]]
+} else {
+    package ifneeded monetdb 0.9.15 \
+	    [list load [file join $dir libmonetdb0.9.15.so] [string totitle monetdb]]
+}
