33ac5652bff18a5b0225f353500501f71e3f4f26
Danilo Chang <ray2501@gmail.com>
2023-12-16 21:06:06 +0800

    Update pkgIndex.tcl.in

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -8,3 +8,6 @@ if {[package vsatisfies [package provide Tcl] 9.0-]} {
     package ifneeded monetdb 0.9.16 \
 	    [list load [file join $dir libmonetdb0.9.16.so] [string totitle monetdb]]
 }
+
+package ifneeded tdbc::monetdb 0.9.16 \
+    [list source [file join $dir tdbcmonetdb.tcl]]
